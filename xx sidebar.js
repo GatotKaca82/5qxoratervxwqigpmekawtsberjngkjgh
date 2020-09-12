8ad0ad5fd898e700ae1969eb763694356e485419
5QX9dYx66FfyWqXCLKi <muhamadkhadafi666@gmail.com>
2020-09-13 04:30:36 +0700

    x

--- xx sidebar.js
+++ xx sidebar.js
@@ -18,7 +18,7 @@ document.write('                                                    <h4 class=\"
 document.write('                                                    </h4>\n');
 document.write('                                                    <div class=\"textwidget custom-html-widget\">\n');
 document.write('                                                        <div align=\"center\"><span\n');
-document.write('                                                                style=\"font-size:115%;\"><strong>0859121530712<br>089618591315<br></strong></span><br><strong><span\n');
+document.write('                                                                style=\"font-size:115%;\"><strong>+62859121530712<br>+6289618591315<br></strong></span><br><strong><span\n');
 document.write('                                                                    style=\"color:blue;\">TELEGRAM</span></strong><br><a\n');
 document.write('                                                                href=\"tg://resolve?domain=ctr_otopulsabot\"><img\n');
 document.write('                                                                    alt=\"cs otopulsa pulsa murah\"\n');
